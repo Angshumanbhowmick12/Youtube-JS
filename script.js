7e1da8dfd073f89036dae39598ae379d8afcc8e6
Angshumanbhowmick12 <angshumanbhowmick1508@gmail.com>
2025-03-21 21:33:03 +0530

    youtube

---
+++ script.js
@@ -0,0 +1,69 @@
+const videolist=document.querySelector(".videos")
+const search=document.querySelector(".btn")
+
+
+
+let video=[]
+
+const fetchData = async()=>{
+    const data =await fetch('https://api.freeapi.app/api/v1/public/youtube/videos?page=1&limit=90&query=javascript&sortBy=keep%2520one%253A%2520mostLiked%2520%257C%2520mostViewed%2520%257C%2520latest%2520%257C%2520oldest')
+    const Json=await data.json()
+
+    console.log(Json.data.data);
+     video= Json.data.data
+    videolist.innerHTML="";
+    allVideos(video)
+}
+
+function allVideos(video){
+    videolist.innerHTML =""
+    video.forEach((video) => {
+        const videoCard = document.createElement("div")
+         videoCard.className="video-card"
+         videoCard.onclick=()=> openVideo(video.items.id)
+         let thumbnail=video?.items?.snippet?.thumbnails?.high?.url
+         //console.log(thumbnail);
+         
+        videoCard.innerHTML=`
+        <img src="${thumbnail}" alt="${video?.items?.snippet?.title}">
+        <h3>${video?.items?.snippet?.title}</h3>
+        <p>${video?.items?.snippet?.channelTitle}</p>
+        <p><span>Views :${video?.items?.statistics?.viewCount/1000}K</span> <span>Likes :${video?.items?.statistics?.likeCount}</span></p>
+        `
+        videolist.appendChild(videoCard)
+    });
+    
+
+
+}
+ function openVideo(videoId){
+    window.open(`https://www.youtube.com/watch?v=${videoId}`, "_blank")
+ }
+
+search.addEventListener('click',()=>{
+
+    console.log(video);
+
+    const text=document.querySelector(".search-text")
+
+    console.log(text.value);
+    
+    
+    const filtered= video.filter((video)=>
+        
+        video?.items?.snippet?.title.toLowerCase().includes(text.value.toLowerCase() || video?.items?.snippet?.channelTitle.toLowerCase().includes(text.value.toLowerCase() )
+    
+    ))
+
+    
+    
+
+    // console.log(filtered);
+    allVideos(filtered)
+})
+
+
+
+
+
+fetchData()
